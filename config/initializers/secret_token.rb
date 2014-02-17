a82d7b563b21c58c264c9b1c6ef34f7548a802a8
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2014-02-16 23:45:55 -0600

    Initial Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetrackr::Application.config.secret_token = 'a024cda4cbad1a0bbbd924d82819e3cab5e632e6a1d8078da6bc36effa561cfe4727b4155d62354cbea9e93166d0f4cf27f6fbaea16235b5f38d2caa06cfc819'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Timetrackr::Application.config.session_store :cookie_store, key: '_timetrackr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Timetrackr::Application.config.session_store :active_record_store
