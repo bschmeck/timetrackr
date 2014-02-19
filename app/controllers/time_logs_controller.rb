a597f51f1b0ca22173b10045c555b1e0620ac2e1
Ben Schmeckpeper <ben@aisle50.com>
2014-02-19 08:21:05 -0600

    The TimeLogsController requires an authenticated user.

--- app/controllers/time_logs_controller.rb
+++ app/controllers/time_logs_controller.rb
@@ -1,4 +1,6 @@
 class TimeLogsController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @current_task = "Email Triage"
     @tasks = [Task.new(name: "Task One"), Task.new(name: "Task Two"), Task.new(name: "Task Three"), Task.new(name: "Task Four")]
