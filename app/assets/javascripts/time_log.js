d2107acc35f7117ddf745bfc644bb3ac206a6e1f
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2014-02-19 21:20:57 -0600

    Toggle the Start/Finish button

--- app/assets/javascripts/time_log.js
+++ app/assets/javascripts/time_log.js
@@ -5,12 +5,16 @@ Trackr.TimeLog = function (id, creation, completion) {
         this.id = id;
     this.creation = creation;
     this.completion = completion;
+    this.$start_button = $("#btn_start");
+    this.$finish_button = $("#btn_finish");
 };
 
 Trackr.TimeLog.prototype.start = function() {
     var me = this;
     $.ajax({url: this.creation, type: "POST"}).done(function(data){
         me.id = data["log_id"];
+        me.$start_button.hide();
+        me.$finish_button.show();
     });
 };
 
@@ -18,6 +22,8 @@ Trackr.TimeLog.prototype.finish = function() {
     var me = this;
     var url = this.completion.replace("/:id/", "/" + this.id + "/");
     $.ajax({url: url, type: "POST"}).done(function(data){
+        me.$start_button.show();
+        me.$finish_button.hide();
         alert("finished working");
     });
 };
