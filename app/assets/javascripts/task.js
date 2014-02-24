1e82b6f1b10f3df419b4b4b0da3e54f2e217ede9
Ben Schmeckpeper <ben@aisle50.com>
2014-02-24 10:14:16 -0600

    Need to actually configure the URL and pass the task name as a parameter.

--- app/assets/javascripts/task.js
+++ app/assets/javascripts/task.js
@@ -6,7 +6,8 @@ Trackr.Task = function(name, id) {
 };
 
 Trackr.Task.create = function(name, callback) {
-    $.ajax({url: Trackr.config["task_create_url"], type: "POST"}).done(function(data){
+    var payload = {name: name};
+    $.ajax({url: Trackr.config["task_create_url"], type: "POST", data: payload}).done(function(data){
         task = new Trackr.Task(data["task_id"], data["task_name"]);
         callback(task);
     });
